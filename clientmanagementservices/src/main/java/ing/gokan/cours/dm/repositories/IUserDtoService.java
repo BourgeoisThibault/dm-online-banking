7b6c1359a55dbe6f4dad8827c02c97306c43867b
BOURGEOIS THIBAULT <thibault.bourgeois@etu.u-pec.fr>
2017-10-18 16:14:02 +0200

    refactor and comment code

--- clientmanagementservices/src/main/java/ing/gokan/cours/dm/repositories/IUserDtoService.java
+++ clientmanagementservices/src/main/java/ing/gokan/cours/dm/repositories/IUserDtoService.java
@@ -9,12 +9,54 @@ import java.util.List;
  * Time     21:42
  */
 public interface IUserDtoService {
+
+    /**
+     * Get all users
+     * @return list
+     */
     public List<UserDto> getAllUser();
+
+    /**
+     * Search with firstname and return result
+     * @param fistName
+     * @return
+     */
     public List<UserDto> getUserByFirstName(String fistName);
+
+    /**
+     * Search with lastname and return result
+     * @param lastName
+     * @return
+     */
     public List<UserDto> getUserByLastName(String lastName);
+
+    /**
+     * Search with firstname ans lastname and return result
+     * @param userDto
+     * @return
+     */
     public UserDto getUserByUser(UserDto userDto);
+
+    /**
+     * Add in list if not existing
+     * @param userDto
+     * @return result
+     */
     public String addUser(UserDto userDto);
+
+    /**
+     * Change in list in exist and if nex not existing
+     * @param userDto
+     * @param newUserDto
+     * @return result
+     */
     public String updateUser(UserDto userDto,UserDto newUserDto);
+
+    /**
+     * Delete from list if exist
+     * @param userDto
+     * @return reuslt
+     */
     public String deleteUser(UserDto userDto);
 
     public boolean contains(UserDto userDto);
